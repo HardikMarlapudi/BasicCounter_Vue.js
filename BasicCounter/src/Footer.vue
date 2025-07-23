f72400693ed2585e5254d4cd3b9b86ebc4f335c3
Hardik Marlapudi <hardikmarlapudi@Hardiks-MacBook-Pro.local>
2025-07-23 02:08:10 -0400

    fixed footer

--- BasicCounter/src/Footer.vue
+++ BasicCounter/src/Footer.vue
@@ -2,12 +2,16 @@
     <footer>&copy; 2025 Hardik Marlapudi. All Rights Reserved.</footer>
 </template>
 
+<script>
+
+</script>
 
 <style>
     footer {
         display: block;
         text-align: center;
-        padding: 670px 20px;
+        margin: 0 auto;
+        padding: 600px 15px;
         font-size: 15px;
         font-family: 'Courier New', Courier, monospace;
         font-weight: bold;
